cf9561013f7c8a1b1216d46caad831be36d24927
Evan Busse <evanbusse@gmail.com>
2016-10-13 15:34:21 -0600

    Completed Stoplight exercise

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+'use strict';
 /*
   Write JS to make this stoplight work.
 
@@ -8,3 +9,32 @@
   When I click on the 'go' button
     the bottom light should turn green.
 */
+var stopLight = document.getElementById('stopLight');
+var slowLight = document.getElementById('slowLight');
+var goLight = document.getElementById('goLight');
+
+function reset () {
+  var color = "background-color: #111";
+  stopLight.style.cssText = color;
+  slowLight.style.cssText = color;
+  goLight.style.cssText = color;
+}
+
+var stopButton = document.getElementById('stopButton');
+var slowButton = document.getElementById('slowButton');
+var goButton = document.getElementById('goButton');
+
+stopButton.addEventListener("click", function() {
+  reset();
+  stopLight.style.cssText = "background-color: red";
+});
+
+slowButton.addEventListener("click", function() {
+  reset();
+  slowLight.style.cssText = "background-color: yellow";
+});
+
+goButton.addEventListener("click", function(){
+  reset();
+  goLight.style.cssText = "background-color: green";
+});
